ffe6a6ce8c638c1cdae28f4e6ae973b7194ec705
TimLanglois <tim.langlois@uwa.edu.au>
2019-02-19 21:06:38 +0800

    add to gitnore

--- Analysis_Lobster_pot distribution/1_Lobster.pot.distribution_import and plot.R
+++ Analysis_Lobster_pot distribution/1_Lobster.pot.distribution_import and plot.R
@@ -16,8 +16,6 @@ rm(list=ls())
 
 
 
-if(!requireNamespace("devtools")) install.packages("devtools")
-devtools::install_github("dkahle/ggmap", ref = "tidyup",force = TRUE)
 
 
 # load librarys----
@@ -25,9 +23,10 @@ library(tidyr) #to tidy data
 library(dplyr) #to transform data
 library(readr) #to write data
 library(ggplot2) #to plot data
-# install.packages("ggmap") #Run once then # out
-
+# if(!requireNamespace("devtools")) install.packages("devtools")
+# devtools::install_github("dkahle/ggmap", ref = "tidyup",force = TRUE)
 library(ggmap)
+library(magrittr)
 
 
 
@@ -56,7 +55,8 @@ plot.dir=paste(work.dir,"Plots",sep="/")
 # Read in polygons of NTR------
 setwd(data.dir)
 dir()
-ntr<-read_csv("ntr.2019-01-23.csv")
+ntr<-read_csv("ntr.2019-01-23.csv")%>%
+  glimpse()
 
 
 # Read in pot data----
@@ -68,14 +68,16 @@ dat<-read_csv("lobster.pots.2019-01-23.csv")%>%
 # Basic plots to check the data-----
 setwd(plot.dir)
 # Get map of Rottnest
-bbox <- c(115.43,-32.04,  115.58,-31.975)
-rotto.map<-(get_stamenmap(as.vector(bbox), zoom = 12, maptype="terrain"))
+bbox <- c(115.435,-32.04,  115.57,-31.975)
+rotto.map<-(get_stamenmap(as.vector(bbox), zoom = 14, maptype="terrain"))
 
+
+ggmap(rotto.map)
 
 # Plot of pots
 
 ggmap(rotto.map)+
-  geom_point(aes(lon,lat),size=2,colour="Yellow",data=dat, alpha=0.25)+
+  # geom_point(aes(lon,lat),size=2,colour="Yellow",data=dat, alpha=0.25)+
   xlab('Longitude')+
   ylab('Lattitude')
 
@@ -83,13 +85,31 @@ ggmap(rotto.map)+
 # Plot of pots with NTRs----
 
 ggmap(rotto.map)+
-  geom_point(aes(lon,lat,colour=zone),size=1,data=dat, alpha=0.25)+
-  geom_polygon(data = fortify(ntr),
-               aes(lon, lat, group = group),
-               fill = "orange", colour = "red", alpha = 0.2) +
-  xlab('Longitude')+
-  ylab('Lattitude')+
-  facet_wrap(~year,ncol=2)
+  geom_point(aes(lon,lat,colour="red"),size=1,data=dat, alpha=0.25,show.legend = F)+
+  # geom_polygon(data = fortify(ntr), aes(lon, lat, group = group), colour = "green", fill="orange",alpha = 0.5) +
+  
+  geom_polygon(data = fortify(ntr%>%filter(lon<115.48)), aes(lon, lat, group = group), colour = "green", fill="orange",alpha = 0.5) +
+  geom_polygon(data = fortify(ntr%>%filter(lon>115.48)), aes(lon, lat, group = group), colour = "green", fill="green",alpha = 0.25) +
+  
+  # xlim(115.43,115.57)+
+  # xlab(' ')+
+  # ylab(' ')+
+  theme(axis.text=element_blank(),
+        
+        axis.title=element_blank(),
+        axis.ticks=element_blank())
+# +
+#   facet_wrap(~year,ncol=2)
+
+
+# Save the plot----
+setwd(plot.dir)
+dir()
+
+ggsave(file="lobster.pots.png", width = 21, height = 10,units = "cm")
+ggsave(file="rotto.png", width = 21, height = 10,units = "cm")
+ggsave(file="rotto.nts.png", width = 21, height = 10,units = "cm")
+
 
 
 
@@ -100,12 +120,22 @@ glimpse(dat)
 
 
 # Plot with distance to NTR boundary-
-ggplot(data=dat, aes(x=dst_sank))+
+labels <- c(N = "North", S = "South")
+
+ggplot(data=dat%>%filter(!zone=="W"), aes(x=dst_sank))+
   geom_density(alpha=.5)+
-  facet_grid( .~ zone)
+  facet_grid( .~ zone,labeller=labeller(zone = labels))+
+  theme_classic()+
+  xlab('Distance to sanctuary (m)')+
+  ylab('Density')+
+  Theme1
 
-
-
+  # Save the plot----
+  setwd(plot.dir)
+  dir()
+  
+  ggsave(file="lobster.distance.png", width = 20, height = 10,units = "cm")
+  
 
 
 # Plot with depth-
@@ -143,6 +173,33 @@ pot.cst.hist
 
 
 
+
+
+
+
+
+# Set Themes for plotting ----
+Theme1 <-
+  theme( # use theme_get() to see available options
+    panel.grid.major = element_blank(), 
+    panel.grid.minor = element_blank(), 
+    legend.background = element_rect(fill="white"),
+    legend.key = element_blank(), # switch off the rectangle around symbols in the legend
+    legend.text = element_text(size=10),
+    legend.title = element_blank(),
+    legend.position = "top",
+    legend.direction="horizontal",
+    text=element_text(size=10),
+    # strip.text.y = element_text(size = 10,angle = 0),
+    axis.title.x=element_text(vjust=0.3, size=12),
+    axis.title.y=element_text(vjust=0.6, angle=90, size=12),
+    axis.text.x=element_text(size=10,angle = 0),
+    axis.text.y=element_blank(),
+    axis.line.x=element_line(colour="black", size=0.5,linetype='solid'),
+    axis.line.y=element_line(colour="black", size=0.5,linetype='solid'),
+    strip.text.y = element_text(size = 10,angle = 0),
+    strip.text.x = element_text(size = 12,angle = 0),
+    strip.background = element_blank())
 
 
 
